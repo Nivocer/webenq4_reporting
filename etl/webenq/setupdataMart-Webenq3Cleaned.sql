ae82a58f6570b471a6ff9acb30d48b91da2370df
Jaap-Andre de Hoop <j.dehoop@nivocer.com>
2013-10-23 12:30:55 +0200

    drop tables in seperatete file and step.

--- etl/webenq/setupdataMart-Webenq3Cleaned.sql
+++ etl/webenq/setupdataMart-Webenq3Cleaned.sql
@@ -1,17 +1,3 @@
-DROP TABLE IF EXISTS fact_answer;
-DROP TABLE IF EXISTS stage_lookup_respondent;
-DROP TABLE IF EXISTS stage_lookup_question;
-DROP TABLE IF EXISTS stage_lookup_answer_label;
-DROP TABLE IF EXISTS lookup_answer_label;
-DROP TABLE IF EXISTS dim_question;
-DROP TABLE IF EXISTS dim_project;
-DROP TABLE IF EXISTS dim_training;
-DROP TABLE IF EXISTS dim_date;
-DROP TABLE IF EXISTS dim_respondent;
-DROP TABLE IF EXISTS dim_country;
-DROP TABLE IF EXISTS dim_questionnaire;
-
-
 CREATE TABLE IF NOT EXISTS stage_lookup_respondent (
                 respondent_id INT NOT NULL,
                 date_import DATE NOT NULL,
---
+++ etl/webenq/setupdataMart-Webenq3DropTables.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS fact_answer;
+DROP TABLE IF EXISTS stage_lookup_respondent;
+DROP TABLE IF EXISTS stage_lookup_question;
+DROP TABLE IF EXISTS stage_lookup_answer_label;
+DROP TABLE IF EXISTS lookup_answer_label;
+DROP TABLE IF EXISTS dim_question;
+DROP TABLE IF EXISTS dim_project;
+DROP TABLE IF EXISTS dim_training;
+DROP TABLE IF EXISTS dim_date;
+DROP TABLE IF EXISTS dim_respondent;
+DROP TABLE IF EXISTS dim_country;
+DROP TABLE IF EXISTS dim_questionnaire;
