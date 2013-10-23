d2de8a2cc430410085c51c77ff2105740ad45384
Jaap-Andre de Hoop <j.dehoop@nivocer.com>
2013-10-23 18:00:34 +0200

    several adjustments
    * keys in dimension to higher hierachy level (question -> questionnaire)
    * update/delete rules to cascade (if you delete a questionnaire, all
        information (except stage-lookup tables) is deleted
    * no create table if not exists, tables are dropped in seperate sql
    
    * sql is now direct output from architect (no cleanup to do)

--- etl/webenq/setupdataMart-Webenq3Cleaned.sql
+++ etl/webenq/setupdataMart-Webenq3Cleaned.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS stage_lookup_respondent (
+
+CREATE TABLE stage_lookup_respondent (
                 respondent_id INT NOT NULL,
                 date_import DATE NOT NULL,
                 respondent_key INT NOT NULL,
@@ -11,7 +12,7 @@ CREATE TABLE IF NOT EXISTS stage_lookup_respondent (
 );
 
 
-CREATE TABLE IF NOT EXISTS stage_lookup_question (
+CREATE TABLE stage_lookup_question (
                 question_id INT NOT NULL,
                 date_import DATE NOT NULL,
                 answer_type VARCHAR(255) NOT NULL,
@@ -22,7 +23,7 @@ CREATE TABLE IF NOT EXISTS stage_lookup_question (
 ALTER TABLE stage_lookup_question MODIFY COLUMN question_id INTEGER COMMENT 'w3: tbl_vragen_id';
 
 
-CREATE TABLE IF NOT EXISTS stage_lookup_answer_label (
+CREATE TABLE stage_lookup_answer_label (
                 question_id INT NOT NULL,
                 answer_id INT NOT NULL,
                 date_import DATE NOT NULL,
@@ -38,26 +39,7 @@ ALTER TABLE stage_lookup_answer_label MODIFY COLUMN answer_id INTEGER COMMENT 'w
 ALTER TABLE stage_lookup_answer_label MODIFY COLUMN answer INTEGER COMMENT 'w3: tbl_antwoordmogelijkheden: antw_nummer';
 
 
-CREATE TABLE IF NOT EXISTS lookup_answer_label (
-                answer_label_key INT AUTO_INCREMENT NOT NULL,
-                answer_label TEXT NOT NULL,
-                missing_answer INT,
-                PRIMARY KEY (answer_label_key)
-);
-
-
-CREATE TABLE IF NOT EXISTS dim_question (
-                question_key INT AUTO_INCREMENT NOT NULL,
-                group_title_en VARCHAR(255),
-                question_text_en TEXT,
-                sequence BIGINT NOT NULL,
-                PRIMARY KEY (question_key)
-);
-
-ALTER TABLE dim_question MODIFY COLUMN sequence BIGINT COMMENT 'order in questionnaire';
-
-
-CREATE TABLE IF NOT EXISTS dim_project (
+CREATE TABLE dim_project (
                 project_key INT AUTO_INCREMENT NOT NULL,
                 project_id INT NOT NULL,
                 project_text_en VARCHAR(255) NOT NULL,
@@ -65,7 +47,7 @@ CREATE TABLE IF NOT EXISTS dim_project (
 );
 
 
-CREATE TABLE IF NOT EXISTS dim_training (
+CREATE TABLE dim_training (
                 training_key INT AUTO_INCREMENT NOT NULL,
                 training_id INT,
                 training_text_en VARCHAR(255),
@@ -73,7 +55,7 @@ CREATE TABLE IF NOT EXISTS dim_training (
 );
 
 
-CREATE TABLE IF NOT EXISTS dim_date (
+CREATE TABLE dim_date (
                 date_key INT NOT NULL,
                 date DATE,
                 year INT NOT NULL,
@@ -91,7 +73,7 @@ ALTER TABLE dim_date MODIFY COLUMN date DATE COMMENT 'date at 0:00 am';
 ALTER TABLE dim_date MODIFY COLUMN month INTEGER COMMENT 'month-nummer 1-12';
 
 
-CREATE TABLE IF NOT EXISTS dim_respondent (
+CREATE TABLE dim_respondent (
                 respondent_key INT AUTO_INCREMENT NOT NULL,
                 respondent_id_external INT NOT NULL,
                 respondent_name VARCHAR(255),
@@ -101,7 +83,7 @@ CREATE TABLE IF NOT EXISTS dim_respondent (
 );
 
 
-CREATE TABLE IF NOT EXISTS dim_country (
+CREATE TABLE dim_country (
                 country_key INT AUTO_INCREMENT NOT NULL,
                 country_code2 VARCHAR(2) NOT NULL,
                 country_code3 VARCHAR(3) NOT NULL,
@@ -114,7 +96,7 @@ ALTER TABLE dim_country COMMENT 'dimension for handling country and region';
 ALTER TABLE dim_country MODIFY COLUMN country_code3 VARCHAR(3) COMMENT '3 character country code';
 
 
-CREATE TABLE IF NOT EXISTS dim_questionnaire (
+CREATE TABLE dim_questionnaire (
                 questionnaire_key INT AUTO_INCREMENT NOT NULL,
                 questionnaire_title_en VARCHAR(255) NOT NULL,
                 questionnaire_id_external INT NOT NULL,
@@ -126,15 +108,36 @@ CREATE TABLE IF NOT EXISTS dim_questionnaire (
 ALTER TABLE dim_questionnaire MODIFY COLUMN sequence INTEGER COMMENT 'order in questionnaire';
 
 
-CREATE TABLE IF NOT EXISTS fact_answer (
+CREATE TABLE dim_question (
+                question_key INT AUTO_INCREMENT NOT NULL,
+                questionnaire_key INT NOT NULL,
+                group_title_en VARCHAR(255),
+                question_text_en TEXT,
+                sequence BIGINT NOT NULL,
+                PRIMARY KEY (question_key)
+);
+
+ALTER TABLE dim_question MODIFY COLUMN sequence BIGINT COMMENT 'order in questionnaire';
+
+
+CREATE TABLE lookup_answer_label (
+                answer_label_key INT AUTO_INCREMENT NOT NULL,
+                question_key INT,
+                answer_label TEXT NOT NULL,
+                missing_answer INT,
+                PRIMARY KEY (answer_label_key)
+);
+
+
+CREATE TABLE fact_answer (
                 respondent_key INT NOT NULL,
                 question_key INT NOT NULL,
                 answer_label_key INT NOT NULL,
                 date_key INT NOT NULL,
                 questionnaire_key INT NOT NULL,
-                country_key INT,
-                project_key INT,
-                training_key INT,
+                country_key INT NOT NULL,
+                project_key INT NOT NULL,
+                training_key INT NOT NULL,
                 answer DOUBLE PRECISION DEFAULT NULL,
                 answer_string TEXT,
                 PRIMARY KEY (respondent_key, question_key, answer_label_key)
@@ -143,51 +146,62 @@ CREATE TABLE IF NOT EXISTS fact_answer (
 ALTER TABLE fact_answer MODIFY COLUMN questionnaire_key INTEGER COMMENT 'link to question table';
 
 
-ALTER TABLE fact_answer ADD CONSTRAINT lookup_answer_label_fact_answer_fk
-FOREIGN KEY (answer_label_key)
-REFERENCES lookup_answer_label (answer_label_key)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE fact_answer ADD CONSTRAINT dim_question_fact_answer_fk
-FOREIGN KEY (question_key)
-REFERENCES dim_question (question_key)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
 ALTER TABLE fact_answer ADD CONSTRAINT dim_project_fact_answer_fk
 FOREIGN KEY (project_key)
 REFERENCES dim_project (project_key)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ON DELETE CASCADE
+ON UPDATE CASCADE;
 
 ALTER TABLE fact_answer ADD CONSTRAINT dim_training_fact_answer_fk
 FOREIGN KEY (training_key)
 REFERENCES dim_training (training_key)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ON DELETE CASCADE
+ON UPDATE CASCADE;
 
 ALTER TABLE fact_answer ADD CONSTRAINT dim_date_fact_answer_fk
 FOREIGN KEY (date_key)
 REFERENCES dim_date (date_key)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ON DELETE CASCADE
+ON UPDATE CASCADE;
 
 ALTER TABLE fact_answer ADD CONSTRAINT fact_answer_dim_respondent_fk
 FOREIGN KEY (respondent_key)
 REFERENCES dim_respondent (respondent_key)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ON DELETE CASCADE
+ON UPDATE CASCADE;
 
 ALTER TABLE fact_answer ADD CONSTRAINT fact_answer_dim_custom_country_fk
 FOREIGN KEY (country_key)
 REFERENCES dim_country (country_key)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ON DELETE CASCADE
+ON UPDATE CASCADE;
 
 ALTER TABLE fact_answer ADD CONSTRAINT dim_questionnaire_fact_answer_fk
 FOREIGN KEY (questionnaire_key)
 REFERENCES dim_questionnaire (questionnaire_key)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ON DELETE CASCADE
+ON UPDATE CASCADE;
 
+ALTER TABLE dim_question ADD CONSTRAINT dim_question_dim_questionnaire_fk
+FOREIGN KEY (questionnaire_key)
+REFERENCES dim_questionnaire (questionnaire_key)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+ALTER TABLE fact_answer ADD CONSTRAINT dim_question_fact_answer_fk
+FOREIGN KEY (question_key)
+REFERENCES dim_question (question_key)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+ALTER TABLE lookup_answer_label ADD CONSTRAINT dim_question_lookup_answer_label_fk
+FOREIGN KEY (question_key)
+REFERENCES dim_question (question_key)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+ALTER TABLE fact_answer ADD CONSTRAINT lookup_answer_label_fact_answer_fk
+FOREIGN KEY (answer_label_key)
+REFERENCES lookup_answer_label (answer_label_key)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
